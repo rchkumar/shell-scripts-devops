4065e58c47a5746bc96622410c2bf4e88228cfac
EC2 Default User <ec2-user@ip-172-31-56-249.ec2.internal>
2021-09-02 11:38:09 +0000

    Making changes to mongo db script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,7 +9,7 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
 
 yum install -y mongodb-org 
 
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 
 

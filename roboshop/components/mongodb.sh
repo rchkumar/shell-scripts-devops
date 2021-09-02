4b58d68788d57a5ec8527409405ee7e343bfbe5f
EC2 Default User <ec2-user@ip-172-31-56-249.ec2.internal>
2021-09-02 12:13:56 +0000

    added the files

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Setting up Mongo DB Repo:"
+
 echo "installing mongodb"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -7,19 +9,32 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
+echo "Installing MongoDB"
+
 yum install -y mongodb-org >/tmp/log
+
+echo "Configuring MongoDB"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 
 
 systemctl enable mongod
-
 systemctl restart mongod
+
+
+echo "Downloading mongodb schema"
 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
+
+echo "Extracting Schema Archive"
+
 unzip mongodb.zip >/tmp/log
+
 cd mongodb-main
+
+echo "Loading Schema"
+
 mongo < catalogue.js >/tmp/log
 mongo < users.js  >/tmp/log

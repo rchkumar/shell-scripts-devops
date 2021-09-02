cfb4308774615d178ea492afc839075e3ade2387
EC2 Default User <ec2-user@ip-172-31-56-249.ec2.internal>
2021-09-02 11:14:58 +0000

    added the files

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 echo "installing mongodb"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
+yum install -y mongodb-org 
+
+#sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
+#Update List IP Address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file : /etc/mongd.conf
+
+
+#systemctl enable mongod
+
+#systemctl restart mongod
+
+#curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+#cd /tmp
+#unzip mongodb.zip
+#cd mongodb-main
+#mongo < catalogue.js
+#mongo < users.js 

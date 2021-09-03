661154d63bebe61a1dce4e8039fe26d2f07841a8
EC2 Default User <ec2-user@ip-172-31-56-249.ec2.internal>
2021-09-03 09:14:58 +0000

    added the scripts

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+Stats_Check(){
+    
+    
+    if [ $1 -eq 0 ]
+    then
+    echo -e "\e[32mSUCCESS\e[0m"
+    else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+    fi
+}
+
 echo "Setting up Mongo DB Repo:"
 
 echo "installing mongodb"
@@ -8,64 +21,32 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-exit 2
-fi
+Stats_Check $?
 
 
 echo "Installing MongoDB"
 
 yum install -y mongodb-org >/tmp/log
 
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-exit 2
-fi
+Stats_Check $?
 
 
 echo "Configuring MongoDB"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-exit 2
-fi
+Stats_Check $?
 
 
 
 systemctl enable mongod
 systemctl restart mongod
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-exit 2
-fi
-
+Stats_Check $?
 
 echo "Downloading mongodb schema"
 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-exit 2
-fi
+Stats_Check $?
 
 
 cd /tmp
@@ -74,13 +55,7 @@ echo "Extracting Schema Archive"
 
 unzip -o mongodb.zip >/tmp/log
 
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-exit 2
-fi
+Stats_Check $?
 
 
 cd mongodb-main
@@ -89,11 +64,6 @@ echo "Loading Schema"
 
 mongo < catalogue.js >/tmp/log
 mongo < users.js  >/tmp/log
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mSUCCESS\e[0m"
-else
-echo -e "\e[31mFAILURE\e[0m"
-fi
+Stats_Check $?
 
-exit
+exit 0

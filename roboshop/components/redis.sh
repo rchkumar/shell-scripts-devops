54bb6e4c7d03b92b7a5e6d162b9f84b5e5136694
EC2 Default User <ec2-user@ip-172-31-56-249.ec2.internal>
2021-09-04 11:20:11 +0000

    added the files..

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -8,23 +8,23 @@ yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-rele
 
 Stats_Check $?
 
-Print "Setup Redis Repos"
+Print "Setup Redis Repos\t\t\t"
 
 yum-config-manager --enable remi &>>$LOG
 
 Stats_Check $?
 
-Print "Install Redis Repos"
+Print "Install Redis Repos\t\t\t"
 
 yum install redis -y &>>$LOG
 Stats_Check $?
 
 
-Print "Configure Redis Listen Address"
+Print "Configure Redis Listen Address\t\t\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 Stats_Check $?
 
-Print "Start Redis Service"
+Print "Start Redis Service\t\t\t"
 
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 Stats_Check $?

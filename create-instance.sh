6d8054d13207eec04396f2ef4b7a962a6d7f7635
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-06 09:58:33 +0000

    added create instance file

--- create-instance.sh
+++ create-instance.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 LID="lt-0ee2e09e618f3b506"
 LVER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID, Version=$LVER
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq

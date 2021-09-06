0f7b2f2f06e0500964875e4eefe43d9ca4dc8c17
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-06 12:09:54 +0000

    added the files

--- create-instance.sh
+++ create-instance.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-
 LID="lt-0ee2e09e618f3b506"
+
+
+#!/bin/bash
+
+LID="lt-01034a8f3c0dc4be2"
 LVER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER | jq
+INSTANCE_NAME=$1
+
+if [ -z "${INSTANCE_NAME}" ]; then
+  echo "Input is missing"
+  exit 1
+fi
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Instance $INSTANCE_NAME is already running"
+  exit 0
+fi
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep stopped &>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Instance $INSTANCE_NAME is already created and stopped"
+  exit 0
+fi
+
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+
+sed -e "s/INSTANCE_NAME/$INSTANCE_NAME/" -e "s/INSTANCE_IP/$IP/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z06898362X9V0BT0IVVT9 --change-batch file:///tmp/record.json | jq

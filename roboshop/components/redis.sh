0896d613fff9eedd83f5e164bdb9c2dbe22c7f10
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-07 05:43:29 +0000

    worked on frontend script

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -21,7 +21,20 @@ Stats_Check $?
 
 
 Print "Configure Redis Listen Address\t\t"
+if [ -f /etc/redis.conf ];
+then
+
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+
+fi
+if [ -f /etc/redis/redis.conf ];
+then
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+
+fi
+
+
 Stats_Check $?
 
 Print "Start Redis Service\t\t\t"

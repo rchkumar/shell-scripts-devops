c070549cd5c2c136d6f7d0bcfbdeab39b9bfbbf3
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-07 07:58:49 +0000

    added mysql and shipping files

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -2,50 +2,54 @@
 
 source components/common.sh
 
-Print "Setup MYSQL Repo"
-
+Print "Setup MySQL Repo\t\t"
 echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+Status_Check $?
 
-Stats_Check $?
+Print "Install MYSQL Service\t\t"
+yum remove mariadb-libs -y &>>$LOG && yum install mysql-community-server -y &>>$LOG
+Status_Check $?
 
-Print "Install MYSQL Service"
-yum remove mariadb-libs -y && yum install mysql-community-server -y  &>>$LOG
-Stats_Check $?
+Print "Start MySQL Service\t\t"
+systemctl enable mysqld &>>$LOG  && systemctl start mysqld &>>$LOG
+Status_Check $?
 
-Print "Start MySQL "
-systemctl enable mysqld  &>>$LOG systemctl start mysqld  &>>$LOG
-Stats_Check $?
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-exit
+Print "Reset Default Password\t\t"
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>$LOG
+if [ $? -eq 0 ]; then
+  echo "Root Password is already set" &>>$LOG
+else
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" >/tmp/reset.sql
+  mysql --connect-expired-password -u root -p"${DEFAULT_PASSWORD}" </tmp/reset.sql &>>$LOG
+fi
+Status_Check $?
 
-Now a default root password will be generated and given in the log file.
-# grep temp /var/log/mysqld.log
+Print "Uninstall Password Validate Plugin"
+echo 'show plugins;' | mysql -u root -p"RoboShop@1" 2>/dev/null | grep -i validate_password &>>$LOG
+if [ $? -eq 0 ]; then
+  echo "uninstall plugin validate_password;" >/tmp/pass.sql
+  mysql -u root -p"RoboShop@1" </tmp/pass.sql &>>$LOG
+else
+  echo "Password plugin is already uninstalled" &>>$LOG
+fi
+Status_Check $?
 
-Next, We need to change the default root password in order to start using the database service.
-# mysql_secure_installation
+Print "Download the Schema\t\t"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG
+Status_Check $?
 
-You can check the new password working or not using the following command.
+Print "Extract Schema File\t\t"
+cd /tmp && unzip -o  mysql.zip &>>$LOG
+Status_Check $?
 
-# mysql -u root -p
+Print "Load Schema\t\t\t"
+cd mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql &>>$LOG
+Status_Check $?
 
-Run the following SQL commands to remove the password policy.
-> uninstall plugin validate_password;
-Setup Needed for Application.
-As per the architecture diagram, MySQL is needed by
-
-Shipping Service
-So we need to load that schema into the database, So those applications will detect them and run accordingly.
-
-To download schema, Use the following command
-
-# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-Load the schema for Services.
-
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -u root -pRoboShop@1 <shipping.sql

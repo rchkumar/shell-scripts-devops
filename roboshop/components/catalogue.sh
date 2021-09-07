b26f302f23e7771bc5b0d22435360c802ca66031
rchkukmar <ramcheekoti.hyd@gmail.com>
2021-09-07 06:42:20 +0000

    updated the user and catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,49 +2,6 @@
 
 source components/common.sh
 
-Print "Installing NodeJS\t"
+COMPONENT=catalogue
+NODEJS
 
-yum install nodejs make gcc-c++ -y  &>>$LOG
-Stats_Check $?
-
-Print "Adding RoboShop User\t"
-id roboshop &>>$LOG
-
-if [ $? -eq 0 ];
-then
-echo " User already there , so skipping" &>>$LOG
-else
-useradd roboshop &>>$LOG
-fi
-
-Stats_Check $?
-
-Print "Downloading Catalogue Content"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
-Stats_Check $?
-
-Print "Extracting Catalogue\t"
-
-cd /home/roboshop
-rm -rf catalogue  && unzip /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue
-Stats_Check $?
-
-Print "Download NodeJS Dependencies"
-
-cd /home/roboshop/catalogue
-
-npm install --unsafe-perm &>>$LOG
-
-Stats_Check $?
-
-chown roboshop:roboshop -R /home/roboshop
-
-Print "Update SystemD Service\t"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
-Stats_Check $?
-
-Print "Setup SystemD Service\t"
-
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl restart catalogue &>>$LOG && 
-systemctl enable catalogue &>>$LOG
-Stats_Check $?
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -28,3 +28,72 @@ fi
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+ADD_APP_USER(){
+    
+            Print "Adding RoboShop User\t"
+            id roboshop &>>$LOG
+            
+            if [ $? -eq 0 ];
+            then
+            echo " User already there , so skipping" &>>$LOG
+            else
+            useradd roboshop &>>$LOG
+            fi
+            
+            Stats_Check $?
+}
+
+DOWNLOAD(){
+        
+        Print "Downloading ${COMPONENT} Content"
+        curl -s -L -o /tmp/{COMPONENT}.zip "https://github.com/roboshop-devops-project/{COMPONENT}/archive/main.zip" &>>$LOG
+        Stats_Check $?
+        
+        Print "Extracting ${COMPONENT}\t"
+        
+        cd /home/roboshop
+        rm -rf ${COMPONENT} && unzip /tmp/${COMPONENT}.zip &>>$LOG && mv ${COMPONENT}-main ${COMPONENT}
+        
+}
+
+
+SystemD_Setup(){
+       Print "Update SystemD Service\t"
+        sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+        Stats_Check $?
+        
+        Print "Setup SystemD Service\t"
+        
+        mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service && systemctl daemon-reload && systemctl restart${COMPONENT} &>>$LOG && 
+        systemctl enable ${COMPONENT} &>>$LOG
+        Stats_Check $?
+
+}
+
+NODEJS(){
+        Print "Installing NodeJS\t"
+        
+        yum install nodejs make gcc-c++ -y  &>>$LOG
+        Stats_Check $?
+        
+        ADD_APP_USER
+        DOWNLOAD
+        
+      
+       
+        
+        Print "Download NodeJS Dependencies"
+        
+        cd /home/roboshop/${COMPONENT}
+        
+        npm install --unsafe-perm &>>$LOG
+        
+        Stats_Check $?
+        
+        chown roboshop:roboshop -R /home/roboshop
+        
+        SystemD_Setup
+        
+     
+}
